0d5125d288f7ba45ffe6940dec6cd92457ed77ec
Abhay Parashar <52857736+Abhayparashar31@users.noreply.github.com>
2021-01-01 18:40:58 +0530

    Update rock_paper.py

--- rock_paper.py
+++ rock_paper.py
@@ -1,16 +1,40 @@
 import random
-score = 0
-cpu = 0
+choices = ["Rock", "Paper", "Scissors"]
+computer = random.choice(choices)
+player = False
+cpu_score = 0
+player_score = 0
 while True:
-    user = input("Choose between: Rock, Paper and Scissors  or e for Exit ")
-    choices = ['rock','paper','scissors']
-    if user.lower()==random.choice(choices):
-        print("You Won!")
-        score+=1
-    elif user.lower()=='e':
-        print("Thanks For Playing!!")
-        print(f"Your Score Is: \nPlayer : CPU \n{score} : {cpu}")
+    player = input("Rock, Paper or  Scissors?").capitalize()
+    ## Conditions of Rock,Paper and Scissors
+    if player == computer:
+        print("Tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("You lose!", computer, "covers", player)
+            cpu_score+=1
+        else:
+            print("You win!", player, "smashes", computer)
+            player_score+=1
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", player)
+            cpu_score+=1
+        else:
+            print("You win!", player, "covers", computer)
+            player_score+=1
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("You lose...", computer, "smashes", player)
+            cpu_score+=1
+        else:
+            print("You win!", player, "cut", computer)
+            player_score+=1
+    elif player=='E':
+        print("Final Scores:")
+        print(f"CPU:{cpu_score}")
+        print(f"Plaer:{player_score}")
         break
     else:
-        print("You Lose")
-        cpu+=1
+        print("That's not a valid play. Check your spelling!")
+    computer = random.choice(choices)
